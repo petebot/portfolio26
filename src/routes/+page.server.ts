d93802b8b8f5448347914bf0b1362e7112b6dc8b
Pete Nawara <pete@petenawara.com>
2026-07-22 18:14:37 -0400

    fix(deploy): prerender portfolio homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import type { PageServerLoad } from './$types';
 import { loadPublishedProjects } from '$lib/server/projects';
 
+export const prerender = true;
+
 export const load: PageServerLoad = async () => {
 	const projects = await loadPublishedProjects();
 
